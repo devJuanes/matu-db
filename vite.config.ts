9381a67a94c0f85557039872a64d1a678e0d3b99
José Landázuri <151701508+landazuricode@users.noreply.github.com>
2026-03-03 10:49:15 -0500

    .

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   preview: {
-    port: 3005
+    port: 3005,
+    allowedHosts: true,
   },
   server: {
     hmr: { overlay: false },
